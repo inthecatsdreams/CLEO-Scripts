76e21d1ba0c953de2ecc53792697a5506a159f08
Ilya Belov <inthecatsdreams@vk.com>
2026-03-04 09:11:58 +0100

    added armour

--- health/index.ts
+++ health/index.ts
@@ -3,6 +3,7 @@ const player = new Player(0);
 if (player.isPlaying()) {
     setInterval(() => {
         player.setHealth(100);
-        
+        player.setNeverGetsTired(true);
+        player.addArmour(250);
     }, 250);
 }
