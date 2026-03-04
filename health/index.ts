5319de84d1d04a4c2677b5dcbd91278c183696ac
Ilya Belov <inthecatsdreams@vk.com>
2026-03-04 09:30:55 +0100

    updated the script

--- health/index.ts
+++ health/index.ts
@@ -1,9 +1,17 @@
 import { KeyCode } from "../.config/enums";
 const player = new Player(0);
-if (player.isPlaying()) {
-    setInterval(() => {
-        player.setHealth(100);
-        player.setNeverGetsTired(true);
-        player.addArmour(250);
-    }, 250);
+let hosts = ["gta3_unreal", "vc_unreal", "sa_unreal"];
+if (HOST == hosts[0] || HOST == hosts[1] || HOST == hosts[2]) {
+
+    if (player.isPlaying()) {
+        setInterval(() => {
+            player.setHealth(250);
+            player.setNeverGetsTired(true);
+            player.addArmour(250);
+        }, 100);
+    }
+
+}
+else {
+    exit("Unsupported game");
 }
